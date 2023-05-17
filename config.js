be6ecd4916531c4e34d6b8c92d761db2a1574b96
perryj533 <43793055+perryj533@users.noreply.github.com>
2023-05-17 19:07:04 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+// config.js
+
+Module export = {
+
+// Blockchain RPC URL
+const rpcUrl = 'https://ethereum.publicnode.com';
+
+// Your Token Address
+const tokenAddress = '0x44d13160094b45f39b712843d887939027513129';
+
+// Pair Token Address
+const pairAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+
+const pairTokenTicker = 'USDT';
+
+// Pair coin id on CoinGecko
+const pairCoinId = 'USDT';
+
+// Your token deployment transaction hash
+const deploymentTxn = '0x59082ba5606664e2f8265c6ea6ff80160cd04c32adb1164c1dcb631eb43c7a31';
+
+const liquidityPoolAddress = 'https://pancakeswap.finance/v2/pair/0x44d13160094b45f39b712843d887939027513129/ETH';
+
+// Addresses where tokens do not participate in circulation like owner, reserve wallets, etc
+const lockedWallets = '0x6d63f6790C976136424c4630f19d93Ae7b089856'
+
+};
